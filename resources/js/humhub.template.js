23ab76065aa17d7fdd3c3aa1b9eb89193703e968
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2019-08-21 00:02:14 -0400

    Update humhub.template.js

--- resources/js/humhub.template.js
+++ resources/js/humhub.template.js
@@ -1,17 +1,17 @@
-humhub.module('template', function(module, require, $) {
+humhub.module('ripfelli', function(module, require, $) {
 
     var init = function() {
-        console.log('template module activated');
+        console.log('ripfelli module activated');
     };
 
     var hello = function() {
-        alert(module.text('hello')+' - '+module.config.username)
+        alert(module.text('rip')+' - '+module.config.username)
     };
 
     module.export({
         //uncomment the following line in order to call the init() function also for each pjax call
         //initOnPjaxLoad: true,
         init: init,
-        hello: hello
+        rip: rip
     });
 });
