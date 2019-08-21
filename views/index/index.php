aebe5ac51885a7beaddb7b2a11f3d0b85e36b222
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2019-08-21 00:03:46 -0400

    Update index.php

--- views/index/index.php
+++ views/index/index.php
@@ -3,23 +3,23 @@
 use humhub\widgets\Button;
 
 // Register our module assets, this could also be done within the controller
-\humhub\humhub\modules\template\assets\Assets::register($this);
+\humhub\humhub\modules\ripfelli\assets\Assets::register($this);
 
-$displayName = (Yii::$app->user->isGuest) ? Yii::t('TemplateModule.base', 'Guest') : Yii::$app->user->getIdentity()->displayName;
+$displayName = (Yii::$app->user->isGuest) ? Yii::t('RipfelliModule.base', 'Guest') : Yii::$app->user->getIdentity()->displayName;
 
 // Add some configuration to our js module
 $this->registerJsConfig("template", [
     'username' => (Yii::$app->user->isGuest) ? $displayName : Yii::$app->user->getIdentity()->username,
     'text' => [
-        'hello' => Yii::t('TemplateModule.base', 'Hi there {name}!', ["name" => $displayName])
+        'hello' => Yii::t('RipfelliModule.base', 'Hi there {name}!', ["name" => $displayName])
     ]
 ])
 
 ?>
 
-<div class="panel-heading"><strong>Template</strong> <?= Yii::t('TemplateModule.base', 'overview') ?></div>
+<div class="panel-heading"><strong>RIP Felli</strong> <?= Yii::t('RipfelliModule.base', 'overview') ?></div>
 
 <div class="panel-body">
-    <p><?= Yii::t('TemplateModule.base', 'Hello World!') ?></p>
+    <p><?= Yii::t('RipfelliModule.base', 'RIP you will be missed dearly!') ?></p>
 
-    <?=  Button::primary(Yii::t('TemplateModule.base', 'Say Hello!'))->action("template.hello")->loader(false); ?></div>
+    <?=  Button::primary(Yii::t('RipfelliModule.base', 'Say Hello!'))->action("ripfelli.rip")->loader(false); ?></div>
